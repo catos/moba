6b48bd5dda7e3f40aab054db5a74da35863d0d2a
Cato Skogholt <cskogholt@gmail.com>
2017-10-02 14:24:01 +0200

    grass block on top

--- LevelGenerator.gd
+++ LevelGenerator.gd
@@ -3,11 +3,23 @@ extends Node
 onready var TileMap = get_node("/root/Game/TileMap")
 onready var level_data = preload("res://level-2.png").get_data()
 
+const TRANSPARENT_COLOR = Color(0, 0, 0, 0)
 const STONE_COLOR = Color(0, 0, 0, 1)
 
 func draw_level():
 	for x in range(level_data.get_width()):
 		for y in range(level_data.get_height()):
+			# Set block to use
+			var block_id = 0
+			
+			# Get pixel from level_data
 			var pixel = level_data.get_pixel(x, y)
+			
+			# If pixel above is transparent, use grass block
+			if y > 0:
+				var pixel_above = level_data.get_pixel(x, y - 1)
+				if pixel_above == TRANSPARENT_COLOR:
+					block_id = 2
+				
 			if pixel == STONE_COLOR:
-				TileMap.set_cell(x, y, 0)
+				TileMap.set_cell(x, y, block_id)
